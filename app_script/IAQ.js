33a37af43ae795ea226e533eb32ce4579f3dd6b9
Zack Huang <zack@atticedu.com>
2021-01-06 10:33:06 +0800

    add app script code

---
+++ app_script/IAQ.js
@@ -0,0 +1,66 @@
+function doGet(e) {
+    return handleResponse(e);
+}
+
+//Recieve parameter and pass it to function to handle
+
+function doPost(e) {
+    return handleResponse(e);
+}
+
+function handleResponse(request) {
+    var output = ContentService.createTextOutput();
+
+    var createAt = new Date();
+    var pm25 = request.parameter.pm25;
+    var co2 = request.parameter.co2;
+    var id = request.parameter.id;
+
+    //open your Spread sheet by passing id
+    var ss = SpreadsheetApp.openById(id);
+    var sheet = ss.getSheetByName("IAQ");
+
+    if (sheet.getCharts().length === 0) {
+        // 清除試算表內容
+        sheet.clear();
+        // 設定標題列
+        sheet.appendRow(["index", "time", "pm2.5", "co2"])
+        // 新建圖表
+        var embededChartBuilder = sheet.newChart().asLineChart();
+        var chart = embededChartBuilder
+            // 使用第一列作為標題
+            .setOption("useFirstColumnAsDomain", true)
+            /* 設定X軸資料來源 */
+            .addRange(sheet.getRange("B:B"))
+            /* 設定Y軸資料來源 */
+            .addRange(sheet.getRange("C:C"))
+            .addRange(sheet.getRange("D:D"))
+            /* 設定圖表長寬 */
+            .setOption('width', 605)
+            .setOption('height', 420)
+            .setNumHeaders(1)
+            /* 設定圖表繪製位置 */
+            .setPosition(1, 5, 0, 0)
+            // 設定標題
+            .setOption("title", "IAQ")
+            // 設定x軸標題
+            .setOption("hAxis", { title: "時間" })
+            .setOption("legend", { position: "top" })
+            /* 執行上面所有設定 */
+            .build();
+        sheet.insertChart(chart);
+    }
+
+    sheet.appendRow([sheet.getLastRow(), createAt, pm25, co2])
+
+    var callback = request.parameters.callback;
+    if (callback === undefined) {
+        output.setContent(JSON.stringify("Success"));
+    } else {
+        output.setContent(callback + "(" + JSON.stringify("Success") + ")");
+    }
+
+    output.setMimeType(ContentService.MimeType.JSON);
+
+    return output;
+}
